e622c25f9c6c480bbc34e6a00728a87d3d19e732
JMaciasAlvarenga <141448323+JMaciasAlvarenga@users.noreply.github.com>
2023-08-05 21:47:10 +0200

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,7 @@
+# Solicitar numero de unidades deseadas
+Precio_Manzana = 50
+unidades = eval(input ("Ingrese cantidad de unidades que desea comprar\n"))
+Total_venta = Precio_Manzana*unidades
+Total_ventas = "j50"
+print (f"Valor Total a pagar: ${Total_venta}")
+print (f"Valor Total a pagar: ${Total_ventas.title()}")
